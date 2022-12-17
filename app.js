08242631257ab43808f8ded4baa3c180ad19a04a
adeshkumar1 <89107712+adeshkumar1@users.noreply.github.com>
2022-12-17 09:12:26 -0600

    yuh

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://adeshkumar:<Lebr0n23>@cluster0.dlr2yf7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    // database and collection code goes here
+    const db = client.db("sample_guides");
+    const coll = db.collection("planets");  
+    // find code goes here
+    const cursor = coll.find();
+    // iterate code goes here
+    await cursor.forEach(console.log);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
